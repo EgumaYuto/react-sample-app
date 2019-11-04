3b363fcd63fcb7ee60b69351885f83aa93c14519
cabos <yuto.eguma@gmail.com>
2019-11-04 20:43:51 +0900

    enable to write title

--- src/components/TodoForm.tsx
+++ src/components/TodoForm.tsx
@@ -6,23 +6,33 @@ interface Props {
     onTodoSubmit: (todo: TodoViewModel) => void
 }
 
-export class TodoForm extends React.Component<Props> {
+interface State {
+    title: string
+}
+
+export class TodoForm extends React.Component<Props, State> {
 
     constructor(props: Props) {
         super(props);
+        this.state = {title: ''};
     }
 
     handleSubmit = (): void => {
         this.props.onTodoSubmit({
             id: ShortId.generate(),
-            title: 'Added TODO'
+            title: this.state.title
         });
+        this.setState({title: ''});
+    };
+
+    handleInput = (event: React.ChangeEvent<HTMLInputElement>): void => {
+        this.setState({title: event.target.value});
     };
 
     render() {
         return (
             <form onSubmit={this.handleSubmit}>
-                <input type={"text"} />
+                <input type={"text"} value={this.state.title} onChange={this.handleInput} />
                 <button type={"submit"}>Button</button>
             </form>
         )
